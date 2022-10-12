119085421ed4df669cf2b7a14053eb009032683a
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-10-11 21:09:09 -0300

    Update

--- grupo12/wsgi.py
+++ grupo12/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo12.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise  
-application = DjangoWhiteNoise(application)
+#from whitenoise.django import DjangoWhiteNoise  
+#application = DjangoWhiteNoise(application)
 
 """  """
