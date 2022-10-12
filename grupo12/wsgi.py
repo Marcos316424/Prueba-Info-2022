33d7efe5723138c451c67092c021e40368a10f42
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-10-11 23:02:22 -0300

    Update

--- grupo12/wsgi.py
+++ grupo12/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo12.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo12.settings.prod')
 
 application = get_wsgi_application()
 
